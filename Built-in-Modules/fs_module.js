eb8a004749e79977b5b5014508210f6434a0b9e4
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 15:38:26 +0530

    result-sync.txt written

--- Built-in-Modules/fs_module.js
+++ Built-in-Modules/fs_module.js
@@ -3,4 +3,9 @@
 // Synchronous
 const {readFileSync, writeFileSync} = require('fs')
 
-const
+const first = readFileSync('./Built-in-Modules/content/first.txt', 'utf-8')
+const second = readFileSync('./Built-in-Modules/content/second.txt', 'utf-8')
+
+console.log(first, second);
+
+writeFileSync('./Built-in-Modules/content/result-sync.txt', `Here is the result: ${first}, ${second}`)
