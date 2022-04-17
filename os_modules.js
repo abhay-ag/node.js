2c65e205798e74c42d5f452c11ff6b5bf9d4b13f
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 15:15:20 +0530

    os_module basic functions

--- os_modules.js
+++ os_modules.js
@@ -1,5 +1,20 @@
+const { log } = require('console');
 const os = require('os')
 
 // info about current user
 const user = os.userInfo()
 console.log(user);
+
+// uptime of the system in seconds
+
+console.log(`The system uptime: ${os.uptime()} seconds`);
+
+// Important funcitions
+
+const currentOs = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+console.log(currentOs);
