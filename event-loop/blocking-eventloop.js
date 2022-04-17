5652f2ef7a9b0474b6647d5405c674a4116a863b
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 18:29:43 +0530

    request to about page blocked

--- event-loop/blocking-eventloop.js
+++ event-loop/blocking-eventloop.js
@@ -1,7 +1,21 @@
 const http = require('http')
 
-const server = http.createServer((res, req) =>{
-    req.end("Hello Another Server")
+const server = http.createServer((req, res) =>{
+    if(req.url === '/'){
+        res.end('Home Page')
+    }
+    else if(req.url === '/about'){
+        // Blocking code
+        for(let i = 0; i < 1000; i++){
+            for(let j = 0; j < 1000; j++){
+                console.log(`${i}, ${j}`);
+            }
+        }
+        res.end('About Page')
+    }
+    else{
+        res.end("Error 404 Not found")
+    }
 })
 
 server.listen(5000, () => {
