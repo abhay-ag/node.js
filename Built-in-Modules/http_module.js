b0d7087778baffbfa4892ab770a4be1dc9df84fb
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 16:29:39 +0530

    Basic server made

--- Built-in-Modules/http_module.js
+++ Built-in-Modules/http_module.js
@@ -4,8 +4,16 @@ const http = require('http')
 
 // creating a server
 const server = http.createServer((req, res) => {
-    res.write('Welcome to our homepage')
-    res.end()
+    // console.log(req); logs a big object
+    if(req.url === '/'){
+        res.end("Hello User!")
+    }else if(req.url === '/about'){
+        res.end("Here is our short history")
+    }else{
+        res.end(`<h1>OOPS!</h1><a href = '/'>Go to Home Page</a>`)
+    }
+    // res.write('Welcome to our homepage')
+    // res.end()
 })
 
 // Assigning server port
