05acb563fd53f1b6a4ab721c904a486ce5cecf7d
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 15:21:17 +0530

    path_module basic functions | a sample txt file

--- path_module.js
+++ path_module.js
@@ -2,3 +2,16 @@ const path = require('path')
 
 // separator property
 console.log(path.sep);
+
+// join file path
+
+const filePath = path.join('/content', 'subfolder', 'test.txt')
+console.log(filePath);
+
+// basename
+const base = path.basename(filePath)
+console.log(base);
+
+// resolve function
+const absolute = path.resolve(__dirname, 'content' , 'subfolder', 'test.txt')
+console.log(absolute);
