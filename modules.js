7019220fd954411857650c251be8d44eb3a0cced
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 12:57:09 +0530

    functions and module

--- modules.js
+++ modules.js
@@ -1,1 +1,13 @@
-// Modules in node
+// Modules in node - every file is a module in node
+// Encapsulated code (only share minimum)
+
+const john = 'john'
+const peter = 'peter'
+
+const sayHi = (name) =>{
+    console.log(`Hello there ${name}`);
+}
+
+sayHi('Abhay')
+sayHi(john)
+sayHi(peter)
