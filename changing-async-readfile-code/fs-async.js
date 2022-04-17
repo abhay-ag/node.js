82ea109730e126d8989dca2da6ebeebe5faee789
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 18:48:08 +0530

    second cleaner approach

--- changing-async-readfile-code/fs-async.js
+++ changing-async-readfile-code/fs-async.js
@@ -1,21 +1,5 @@
 const { readFile } = require('fs')
-const { result } = require('lodash')
-
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf-8', (err, res) => {
-            if(err){
-                reject(err)
-            }else{
-                resolve(res)
-            }
-        })
-    })
-}
-
-getText('./Built-in-Modules/content/first.txt')
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
+const util = require('util')
 // Old approach required nesting of readFiles
 /*readFile('./Built-in-Modules/content/first.txt', 'utf-8', (err, res) => {
     if(err){
@@ -25,3 +9,38 @@ getText('./Built-in-Modules/content/first.txt')
         console.log(res);
     }
 }))*/
+
+// This approach still hectic as we have to use a wrapper function
+// So we use promisify function from util
+/*const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf-8', (err, res) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(res)
+            }
+        })
+    })
+}*/
+
+const rFile = util.promisify(readFile)
+
+// ===== STILL HECTIC =====
+// getText('./Built-in-Modules/content/first.txt')
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err))
+
+
+const start = async() => {
+    try{
+        const first = await getText('./Built-in-Modules/content/first.txt')         // waiting the promise to be resolved
+        const second = await getText('./Built-in-Modules/content/second.txt')
+        console.log(first, second);
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+start()
