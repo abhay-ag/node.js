2116d443b2f412f340983a1299a8a1d7de5850d5
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 18:39:42 +0530

    using promise | async await

--- changing-async-readfile-code/fs-async.js
+++ changing-async-readfile-code/fs-async.js
@@ -1,10 +1,27 @@
 const { readFile } = require('fs')
+const { result } = require('lodash')
 
-readFile('./Built-in-Modules/content/first.txt', 'utf-8', (err, res) => {
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf-8', (err, res) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(res)
+            }
+        })
+    })
+}
+
+getText('./Built-in-Modules/content/first.txt')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+// Old approach required nesting of readFiles
+/*readFile('./Built-in-Modules/content/first.txt', 'utf-8', (err, res) => {
     if(err){
         console.log(err);
         return
     }else{
         console.log(res);
     }
-})
+}))*/
