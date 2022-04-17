7bfa8dea246fe4f3ade94e00b0e68c93a19d85ab
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 18:08:25 +0530

    even loop example

--- event-loop/event-loop.js
+++ event-loop/event-loop.js
@@ -4,4 +4,19 @@
     What is single threaded?
     Ans. JS reads everything line by line , hence a time consuming task can hang up all the applications.
         1. setTimeout simply offloads the task.
+    
+    We execute the necessary code first and the offloaded code is run when we have time left.
 */
+const { readfile, readFile } = require('fs')
+
+console.log('started a first task');
+// Check file path
+readFile('./Built-in-Modules/content/first.txt', 'utf-8', (err,res) => {
+    if(err){
+        console.log(err);
+        return
+    }
+    console.log(res);
+    console.log('completed first task');
+})
+console.log('starting next task');
