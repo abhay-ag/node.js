b4c0081640000e4d2a92d104258455be2a4acb79
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 15:50:45 +0530

    Async fs_module done

--- Built-in-Modules/fs_module-async.js
+++ Built-in-Modules/fs_module-async.js
@@ -1,1 +1,25 @@
-const { readFile, writeFile } = require('fs')
+const { readFile, writeFile, write } = require('fs')
+
+// parameters: <path> <encoding> <callback>
+readFile('./Built-in-Modules/content/first.txt','utf-8', (err, result) =>{
+    if(err){
+        console.log(err);
+        return
+    }
+    // console.log(result);                // result can be only accessed in this function
+    const first = result;
+    readFile('./Built-in-Modules/content/second.txt', 'utf-8', (err, result) => {
+        if(err){
+            console.log(err);
+            return
+        }
+        const second = result
+        writeFile('./Built-in-Modules/content/result-async.txt', `Here is the result: ${first}, ${second}`, (err, result) => {
+            if(err){
+                console.log(err);
+                return
+            }
+            console.log(result);
+        })
+    })
+})
