398c68d286b8b1b98617047634e2feb7e75bca27
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 12:28:12 +0530

    DIB browser and Node JS

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+    Bowser JS
+        1. DOM
+        2. Interactive Apps
+        3. No fileSystem access
+        4. ES6 Modules
+
+    Node JS
+        1. No DOM
+        2. Server side Apps
+        3. Filesystem access
+        4. Common JS
+*/
