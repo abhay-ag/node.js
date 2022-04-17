cbc6e41b09739cc45fca3cbc3c0e566a7e7a62b1
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 12:44:44 +0530

    GLOBALS in Node

---
+++ global.js
@@ -0,0 +1,10 @@
+
+// Global - not window based
+
+/*
+    __dirname = path to current directory
+    __filename = file name
+    require = function to use modules(CommonJS)
+    moudule = info about current module (file)
+    process = info about env where the program is being executed
+*/
