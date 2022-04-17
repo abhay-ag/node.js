8fad4b7680eddf81e0bfc5c800b567224f730305
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 13:09:14 +0530

    module made

---
+++ Day-1/first_module.js
@@ -0,0 +1,8 @@
+// local
+const secret = 'Super Secret'
+
+// shareable
+const john = 'john'
+const peter = 'peter'
+
+module.exports = {john, peter}      // exporting the minimum
--- Day-1/modules.js
+++ Day-1/modules.js
@@ -1,13 +1,27 @@
 // Modules in node - every file is a module in node
 // Encapsulated code (only share minimum)
 
-const john = 'john'
-const peter = 'peter'
+// const john = 'john'          // copied to first_module.js
+// const peter = 'peter'
 
-const sayHi = (name) =>{
-    console.log(`Hello there ${name}`);
-}
+// const sayHi = (name) =>{
+//     console.log(`Hello there ${name}`);      // copied to utils.js
+// }
+
+const names  = require('./first_module')    // importing names
+const sayHi = require('./utils')            // importing the function
+
+console.log(names);     // logs the object with the data passed
 
 sayHi('Abhay')
-sayHi(john)
-sayHi(peter)
+// sayHi(john)     // error john is note defined
+// sayHi(peter) 
+sayHi(names.peter)
+sayHi(names.john)
+
+
+// benifits of modules:
+    /*
+        1. declutters files
+        2. Diversifies code
+    */
