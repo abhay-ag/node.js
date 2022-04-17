f5521a5fea6335dda9935e59c0f4908e4ee218df
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-17 16:53:45 +0530

    about npm

---
+++ npm Modules/npm.js
@@ -0,0 +1,13 @@
+/*
+    when node is installed npm is installed automatically 
+
+    npm --global 
+    npm --version
+
+    local dependency - uses it only in the current specified project
+    npm i <package name>
+
+    global dependency - use it in any project
+    npm install -g <package name>
+    sudo install -g <package name> [mac and linux]
+*/
