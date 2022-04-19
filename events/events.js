639cb11b49cc558f5375a46959183654e7cafcec
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 12:48:28 +0530

    more about events

--- events/events.js
+++ events/events.js
@@ -5,8 +5,16 @@ const customEmmitter = new EventEmitter()
 // on() listens for specific events 
 // emit() will create an event
 
-customEmmitter.on('response', () =>{        // string name should match in the emit function
-    console.log(`data recieved `);
+customEmmitter.on('response', (name, idNum) =>{        // string name should match in the emit function
+    console.log(`data recieved ${name}: ${idNum}`);
 })
 
-customEmmitter.emit('response')
+// We can have multiple on() with same event name with different functionality.
+// order matters
+
+/*
+    First we have to listen for an event.
+    Second we have to emit the event.
+*/
+
+customEmmitter.emit('response', 'john', 34) // we can pass data also.
