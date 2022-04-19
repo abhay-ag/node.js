800ac9f93534314f3e90e8c3b8186e444d986bbb
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 21:01:00 +0530

    problem with this setup

--- streams/http-stream.js
+++ streams/http-stream.js
@@ -5,3 +5,9 @@ http.createServer((req, res) =>{
     const text = fs.readFileSync('./content/big.txt', 'utf-8')
     res.end(text)
 }).listen(5000)
+
+// problem with the setup
+/*
+    The request size is very large.
+    Large content length
+*/
