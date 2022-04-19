e590eaea65c7633b06b2dec9c1c3d428787c1e39
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 21:07:07 +0530

    new setup is good

--- streams/http-stream.js
+++ streams/http-stream.js
@@ -2,8 +2,19 @@ const http = require('http')
 const fs = require('fs')
 
 http.createServer((req, res) =>{
-    const text = fs.readFileSync('./content/big.txt', 'utf-8')
-    res.end(text)
+    // Problem setup
+    // const text = fs.readFileSync('./content/big.txt', 'utf-8')
+    // res.end(text)
+
+    // New setup
+
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf-8');
+    fileStream.on('open', () => {
+        fileStream.pipe(res)   // pushing from the readstream to the writestream
+    })
+    fileStream.on('error', (err) => {
+        res.end(err)
+    })
 }).listen(5000)
 
 // problem with the setup
@@ -11,3 +22,5 @@ http.createServer((req, res) =>{
     The request size is very large.
     Large content length
 */
+
+// new setup headers ==> chunks`
