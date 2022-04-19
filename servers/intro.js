8d5b8459d763305701285f3cc3066d861ee0292a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 21:14:17 +0530

    about http servers

---
+++ servers/intro.js
@@ -0,0 +1,18 @@
+// we request data and get a response from server
+// done using http protocol hence called http messages
+
+// accomplished using express
+// servers job is to make a resource always available.
+
+/*
+    General structure for http messages:
+        1. Start line       ==> method ,url
+        2. Header
+        3. blank line for metainfo
+        4. body
+*/
+
+// HTTP Method
+/*
+    get, post, put, delete
+*/  
