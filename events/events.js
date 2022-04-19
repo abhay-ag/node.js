343d496e95d9dc1dcaa80fd513093c511f2ccb8b
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-19 12:45:15 +0530

    comment added

--- events/events.js
+++ events/events.js
@@ -5,7 +5,7 @@ const customEmmitter = new EventEmitter()
 // on() listens for specific events 
 // emit() will create an event
 
-customEmmitter.on('response', () =>{
+customEmmitter.on('response', () =>{        // string name should match in the emit function
     console.log(`data recieved `);
 })
 
