4f75587c9f362ee0fdee271bec5c9975a915f831
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-20 19:42:34 +0530

    writeHead(), write() functions

--- Express/express.js
+++ Express/express.js
@@ -1,9 +1,18 @@
 const http = require('http')
 
+/*
+    no data returned to user
+    no headers used
+*/
+
 const server = http.createServer((req, res) => {
     // we have to always use end() function
-    console.log('user hit the server');
-    res.end("<h1>Hello User</h1>")
+    // console.log('user hit the server');
+
+    // the browser knows to render html now
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.write('<h1>Home page</h1>')
+    res.end()
 })
 
 server.listen(8000)
