71de8497a0f8402ecce694c1eed026fbeb1a328a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-20 19:32:32 +0530

    creating a server

--- Express/express.js
+++ Express/express.js
@@ -1,2 +1,9 @@
 const http = require('http')
 
+const server = http.createServer((req, res) => {
+    console.log('user hit the server');
+})
+
+server.listen(8000)
+// listening point
+// 443 for https protocol
