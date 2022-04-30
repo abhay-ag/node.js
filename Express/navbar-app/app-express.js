d4df0391111c8b875e975807f5f9c2140d514942
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-30 12:59:10 +0530

    using static resources in the public folder

---
+++ Express/navbar-app/app-express.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('./public'))    // for easy access of resources || all the static resources
+
+app.get('/', (req, res)=> {
+    // use sendFile instead of send
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.all('*', (req, res)=>{
+    res.status(404).send('<h1>Resource not Found</h1>')
+})
+
+app.listen(5000, () => {
+    console.log('server is listening on the port 5000.......');
+})
