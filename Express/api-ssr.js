782faf906463e110180ef3db860ccb2301769929
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-30 16:26:03 +0530

    basic api made

--- Express/api-ssr.js
+++ Express/api-ssr.js
@@ -9,3 +9,17 @@
     2. Send template
     3. RES.RENDER()     ==> redering data
 */
+
+// most basic api
+
+const express = require('express')
+const app = express();
+
+// sending json data
+app.get('/', (req, res) => {
+    res.status(200).json([{name: "Abhay"}, {name: "Susan"}])
+})
+
+app.listen(5000, ()=>{
+    console.log('Server listening on port 5000......');
+})
