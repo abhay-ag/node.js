59440a5a03eb8096544c8d23ae207f1d6d360f5b
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-30 16:15:37 +0530

    api vs ssr

---
+++ Express/api-ssr.js
@@ -0,0 +1,11 @@
+// API vs SSR
+
+/*
+    1. API is JSON
+    2. Send Data
+    3. RES.JSON()       ==> sending data
+
+    1. SSR is template
+    2. Send template
+    3. RES.RENDER()     ==> redering data
+*/
