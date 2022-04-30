dc2efdb6ff37c7b54b5f056f5b4831db0c86c051
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-30 11:48:20 +0530

    exlpanation about req

--- Express/express.js
+++ Express/express.js
@@ -8,11 +8,17 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     // we have to always use end() function
     // console.log('user hit the server');
+    console.log(req.method);
+    console.log(req.url);
 
     // the browser knows to render html now
+    // provind header/metadata
+                //status code  // headers
     res.writeHead(200, {'content-type': 'text/html'})
+                                        // html is important to render html else plain text will be rendered
     res.write('<h1>Home page</h1>')
     res.end()
+    // must be called on each response
 })
 
 server.listen(8000)
