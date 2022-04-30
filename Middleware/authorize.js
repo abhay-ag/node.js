bdb2cd44aec69843694a9e26c4ad89ba936a40cc
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-30 21:11:29 +0530

    conditional statements in middleware

--- Middleware/authorize.js
+++ Middleware/authorize.js
@@ -1,6 +1,17 @@
 const authorize = (req, res, next) => {
-    console.log('authorized');
-    next()
+
+    // using conditions in middleware
+    const {user} = req.query;
+
+    if(user === 'Abhay'){
+        req.user = {name: 'Abhay', id: 3}
+        next()
+    }else{
+        res.status(401).send('unauthorized')
+    }
+
+    // console.log('authorized');
+    // next()
 }
 
 module.exports = authorize
