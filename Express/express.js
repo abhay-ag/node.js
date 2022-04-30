ce7fc09ff045d9393cb86cad958c00deb33f723b
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-30 12:00:50 +0530

    basic server made

--- Express/express.js
+++ Express/express.js
@@ -8,17 +8,36 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     // we have to always use end() function
     // console.log('user hit the server');
-    console.log(req.method);
-    console.log(req.url);
 
-    // the browser knows to render html now
-    // provind header/metadata
-                //status code  // headers
-    res.writeHead(200, {'content-type': 'text/html'})
-                                        // html is important to render html else plain text will be rendered
-    res.write('<h1>Home page</h1>')
-    res.end()
-    // must be called on each response
+    // req methods
+    // console.log(req.method);
+    // console.log(req.url);
+
+    const url = req.url;
+
+    if(url === '/'){
+        // home page
+        // the browser knows to render html now
+        // provind header/metadata
+                    //status code  // headers
+        res.writeHead(200, {'content-type': 'text/html'})
+                                            // html is important to render html else plain text will be rendered
+        res.write('<h1>Home page</h1>')
+        res.end()
+        // must be called on each response
+    }else if(url === '/about'){
+        // about page
+
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }else{
+        //resource not found - 404
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.write('<h1>Page not found</h1>')
+
+        res.end()
+    }
 })
 
 server.listen(8000)
