e0f4fb488e4bc3c38cf60f92941cc1e10e468308
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-30 12:41:57 +0530

    first server made

--- Express/express-basics.js
+++ Express/express-basics.js
@@ -1,2 +1,21 @@
 const express = require('express');
 const app = express();
+
+
+app.listen(5000, () => {
+    console.log('server listening');
+})
+
+// methods of express
+/*
+    app.get
+    app.post
+
+    app.put
+    app.delete
+
+    app.all
+    app.use
+    app.list
+    app.listen
+*/
