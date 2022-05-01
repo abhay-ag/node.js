92395fca83e5bbe57bc8800d25f20cac899ac7b2
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-01 12:33:33 +0530

    delete method

--- Middleware/http-methods.js
+++ Middleware/http-methods.js
@@ -83,6 +83,21 @@ app.put('/api/people/:id', (req, res) => {
     res.status(200).json({success: true, data: newPeople})
 })
 
+// delete method
+app.delete('/api/people/:id', (req, res) => {
+    const {id} = req.params
+
+    const person = people.find((person) =>  person.id === Number(id))
+    if(!person){
+        return res
+            .status(404)
+            .json({success: false, msg: `No person with id:${id}`})
+    }
+
+    const newPeople = people.filter((person) => person.id !== Number(id))
+    res.status(201).json({success: true, data: newPeople})
+})
+
 
 app.listen(5000, () => {
     console.log('Server listening on the port 5000.....');
