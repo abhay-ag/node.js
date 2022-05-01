df31b17412823b7b25b571a6bd8c52d48e31776f
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-01 11:14:18 +0530

    folder location changed

--- Middleware/http-methods.js
+++ Middleware/http-methods.js
@@ -3,16 +3,17 @@ const app = express()
 
 let {people} = require('../Express/data')
 
-
+// static assests
+app.use(express.static('/methods-public'))
 
 // get method ==> default method the browser performs
 app.get('/api/people', (req, res)=> {
     res.status(200).json({success: true, data: people})
 })
 
-app.all('*', (req, res) => {
-    res.status(404).send('Resource not found..')
-})
+// app.all('*', (req, res) => {
+//     res.status(404).send('Resource not found..')
+// })
 
 app.listen(5000, () => {
     console.log('Server listening on the port 5000.....');
