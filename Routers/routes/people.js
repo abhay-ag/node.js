09cfee9ac5814771e4829c63069e4e5eb3653ea4
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-01 13:27:50 +0530

    chunks deleted

--- Routers/routes/people.js
+++ Routers/routes/people.js
@@ -4,25 +4,10 @@ const routers = express.Router();
 const {getPeople, createPerson, createPersonPostman, updatePerson, deletePerson} = require('../controllers/people')
 
 
-// get method ==> default method the browser performs
-
-// we remove the base from the routers as we have it in the app.js in use
 routers.get('/', getPeople /* function moved to ./controllers/people.js */)
-
-// javascript form
 routers.post('/', createPerson)
-
 routers.post('/postman', createPersonPostman)
-
-
-// routers.all('*', (req, res) => {
-//     res.status(404).send('Resource not found..')
-// })
-
-// put method
 routers.put('/:id', updatePerson)
-
-// delete method
 routers.delete('/:id', deletePerson)
 
 module.exports = routers;
