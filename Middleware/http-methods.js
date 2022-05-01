2a2e83a98b4ba288a536d09a8788c5b88f392475
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-01 11:33:43 +0530

    basic login | checking through post request

--- Middleware/http-methods.js
+++ Middleware/http-methods.js
@@ -4,11 +4,32 @@ const app = express()
 let {people} = require('../Express/data')
 
 // static assests
-app.use(express.static('/methods-public'))
+app.use(express.static('./methods-public'))
+
+// parse form data
+app.use(express.urlencoded({extended: false}))
 
 // get method ==> default method the browser performs
 app.get('/api/people', (req, res)=> {
     res.status(200).json({success: true, data: people})
+})
+
+// post method
+app.post('/login', (req, res) => {
+    // console.log(req.body);
+
+    const {name} = req.body;
+    let flag = false;
+    for (let i = 0; i < people.length; i++){
+        if(name === people[i].name){
+            flag = true;
+        }
+    }
+    if(flag){
+        return res.status(200).send(`Hello, ${name}`)
+    }else{
+        return res.status(401).send('User not found..')
+    }
 })
 
 // app.all('*', (req, res) => {
