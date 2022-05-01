0cc2221baa2a34f61f6b6efb7a4c75a3bf39a20c
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-01 13:29:49 +0530

    smaller way of writing routes

--- Routers/routes/people.js
+++ Routers/routes/people.js
@@ -1,13 +1,18 @@
-const express = require('express')
+const express = require('express');
 const routers = express.Router();
 
 const {getPeople, createPerson, createPersonPostman, updatePerson, deletePerson} = require('../controllers/people')
 
 
-routers.get('/', getPeople /* function moved to ./controllers/people.js */)
-routers.post('/', createPerson)
-routers.post('/postman', createPersonPostman)
-routers.put('/:id', updatePerson)
-routers.delete('/:id', deletePerson)
+// routers.get('/', getPeople /* function moved to ./controllers/people.js */)
+// routers.post('/', createPerson)
+// routers.post('/postman', createPersonPostman)
+// routers.put('/:id', updatePerson)
+// routers.delete('/:id', deletePerson)
+
+// another way of setting up routes.
+routers.route('/').get(getPeople).post(createPerson)
+routers.route('/postman').post(createPersonPostman)
+routers.route('/:id').put(updatePerson).delete(deletePerson)
 
 module.exports = routers;
